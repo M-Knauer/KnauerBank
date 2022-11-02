943630f72af494bc3c56be30c04860d1a57d34bb
unknown <marcelocrisknauer@gmail.com>
2022-11-01 23:57:44 -0300

    dependencias h

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from app.configs.connection import DbConnectionHandler as dbc
 
 app = Flask(__name__)
