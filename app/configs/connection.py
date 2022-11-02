eef4962045f51993023a933037ce93b045b6ada7
unknown <marcelocrisknauer@gmail.com>
2022-11-02 01:17:34 -0300

    db config

--- app/configs/connection.py
+++ app/configs/connection.py
@@ -9,7 +9,7 @@ class DbConnectionHandler:
     def __init__(self) -> None:
         sqlpw = os.environ.get("sqlpw")
         self.__connection_string = \
-            "mysql://b077087ce6d76d:6e9354a5@us-cdbr-east-06.cleardb.net/heroku_6e1c9de7d51b4b7?reconnect=true"
+            "mysql+pymysql://b077087ce6d76d:6e9354a5@us-cdbr-east-06.cleardb.net/heroku_6e1c9de7d51b4b7?reconnect=true"
             #f'mysql+pymysql://root:{sqlpw}@localhost:3306/test_bank'
         self.__engine = self.__create_database_engine()
         self.session = None
